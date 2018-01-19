c6ac39cf8d7774fefdcef42909abdc7241294ae0
陈佳志 <jiazhi.chen@vdin.net>
2018-01-19 10:17:03 +0800

    rabbo=itmq

---
+++ src/main/java/com/chenjazz/rabbitmq/ch1/生产者.java
@@ -0,0 +1,27 @@
+package com.chenjazz.rabbitmq.ch1;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class 生产者 {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.0.83");
+
+        Connection connection = factory.newConnection();
+
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        channel.basicPublish("", QUEUE_NAME, null, "hello world".getBytes("utf-8"));
+        channel.close();
+        connection.close();
+    }
+}
---
+++ src/main/java/com/chenjazz/rabbitmq/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * RabbitMQ入门教程 For Java
+ * http://blog.csdn.net/chwshuang/article/details/50521708
+ */
+package com.chenjazz.rabbitmq;
